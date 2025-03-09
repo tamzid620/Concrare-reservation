bcf10d703aa5d5ec2c0f2301185d3c04f6f4a97b
tamzid620 <tamzid620@gmail.com>
2025-03-09 15:44:31 +0600

    first commit

---
+++ src/app/(home-layout)/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="lg:max-w-6xl md:max-w-3xl sm: max-w-sm mx-auto bg-gray-800">
+        <h2>Home</h2>
+    </div>
+  );
+}
